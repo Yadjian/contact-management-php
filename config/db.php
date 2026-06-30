847b5a4c05d8f2b1f966151e4fe82ef68efc1f09
Lucas-Boyadjian <10462@holbertonstudents.com>
2026-06-30 13:33:35 +0200

    Initialize SQL script and configure PDO connection

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+    
+include 'config.php';
+
+try {
+    $dbh = new PDO("mysql:host=$server;dbname=$dbname", $user, $pass);
+    echo "Connexion réussi !";
+
+} catch (PDOException $e) {
+    echo "Echec de la connexion !" . $e->getMessage();
+}
+
+?>
---
+++ init.sql
@@ -0,0 +1,25 @@
+CREATE DATABASE IF NOT EXISTS contact_manager;
+
+USE contact_manager;
+
+CREATE TABLE IF NOT EXISTS categories (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS contacts (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    email VARCHAR(100),
+    phone VARCHAR(20),
+    city VARCHAR(50),
+    address VARCHAR(100),
+    category_id INT NOT NULL,
+    FOREIGN KEY (category_id) REFERENCES categories(id)
+);
+
+INSERT INTO categories (name) VALUES ('Ami');
+INSERT INTO categories (name) VALUES ('Famille');
+INSERT INTO categories (name) VALUES ('Travail');
+INSERT INTO categories (name) VALUES ('Autre');
