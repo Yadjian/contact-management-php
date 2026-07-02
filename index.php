b636c3147ba010f6ec90c821411fa87b63f5b8b5
Lucas-Boyadjian <10462@holbertonstudents.com>
2026-07-02 11:57:17 +0200

    Finish the controller and the router

--- index.php
+++ index.php
@@ -10,16 +10,31 @@ $categoriesModel = new CategoriesModel($pdo);
 $contactsController = new ContactsController($contactsModel, $categoriesModel);
 
 if (empty($_GET['page'])) {
-    $url[0] = "contacts";
+    $url[0] = 'contacts';
 
 } else {
-    $url = explode("/", filter_var($_GET["page"], FILTER_SANITIZE_URL));
+    $url = explode('/', filter_var($_GET['page'], FILTER_SANITIZE_URL));
 }
 
 switch ($url[0]) {
-    case "mycontacts":
-        $contactsController->listContacts();
-
-    case
+    case 'contacts':
+        if (!isset($url[1])) {
+            $contactsController->list();
+        } else {
+            switch ($url[1]) {
+                case 'add':
+                    $contactsController->add();
+                    break;
+                case 'delete':
+                    $contactsController->delete($_GET['id']);
+                    break;
+                case 'update':
+                    $contactsController->update($_GET['id']);
+            }
+        }
+        break;
+    default:
+        echo "Page introuvable";
+        break;
 }
 ?>
